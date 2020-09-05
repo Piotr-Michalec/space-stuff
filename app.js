16629ce48f89e8bc05dee57cce66e6f954d872bb
Diotr <35659983+Diotr@users.noreply.github.com>
2020-09-05 20:05:01 +0100

    fetch photo of the day from nasa api added, index.ejs created

--- app.js
+++ app.js
@@ -2,18 +2,42 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const dotenv = require('dotenv').config()
 const path = require('path')
+const { response } = require('express')
+
+const NASA_API_KEY = `${process.env.NASA_API}`
+//nasa photo of the day
+const NASA_APOD = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+const fetch = require("node-fetch");
+
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.set('view engine' , 'ejs')
 
+//fetch data from api, pass the url as an argument
+const FetchDataFromApi = async uri =>{
+       let response = await fetch(uri)
+       let data = await response.json()
+       return data
+}
 
+//fetch photo of the day from nasa api
+app.get('/' ,async (req,res)=>{
+    let nasaData = await FetchDataFromApi(NASA_APOD)
+    res.render('index', {nasaData: nasaData})
+    console.log('app get',nasaData)
+})
 
-app.get('/' , (req,res)=>{
-    
-    res.render('index')
+//fetch data
+app.get('/admin' , (req,res)=>{
+    //call FetchDataFromAPi here and pass url as an argument
+    res.render('adminpanel')
 })
 
 
