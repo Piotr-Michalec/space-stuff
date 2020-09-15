28387aaac6a49fe5c110a0d517b8c3b59d55be81
Diotr <35659983+Diotr@users.noreply.github.com>
2020-09-15 20:44:11 +0100

    added mars/info path and other changes

--- public/script.js
+++ public/script.js
@@ -1,73 +1,84 @@
- const sol = '1050'//zrobic sol i musi zwracac bład jeśli nieprawidłowy
+const potdPath = "/potd";
 
-const potdPath = '/potd'
+let description = document.querySelector(".potd-description");
+let photoOfTheDay = document.querySelector(".potd-img");
+let title = document.querySelector(".potd-title");
+let potdHref = document.querySelector(".a-potd");
 
-let description = document.querySelector('.potd-description')
-let photoOfTheDay=document.querySelector('.potd-img')
-let title = document.querySelector('.potd-title')
-let potdHref = document.querySelector('.a-potd')
+let roverGallery = document.querySelector(".rover-image-gallery");
+let roverImage = document.querySelector(".image-from-rover");
 
-let roverGallery = document.querySelector('.rover-image-gallery')
-let roverImage = document.querySelector('.image-from-rover')
+let setSolBtn = document.querySelector(".set-sol");
 
+const fetchDataFromApi = async (path) => {
+  let response = await fetch(path);
+  let data = await response.json();
+  return data;
+};
 
-const fetchDataFromApi = async (path) =>{
-    let response = await fetch(path)
-    let data = await response.json()
-    return data
-}
-
-
-const curiosityBtn = document.querySelector('#curiosity-rover')
-const opportunityBtn = document.querySelector('#opportunity-rover')
-const spiritBtn = document.querySelector('#spirit-rover')
+const curiosityBtn = document.querySelector("#curiosity-rover");
+const opportunityBtn = document.querySelector("#opportunity-rover");
+const spiritBtn = document.querySelector("#spirit-rover");
 
 //get nasa photo of the day
-let setPhotoOfTheDay = data =>{
-   description.textContent =  data.nasaData.explanation
-   photoOfTheDay.src=data.nasaData.hdurl
-   potdHref.setAttribute('href',data.nasaData.hdurl )
-   title.textContent=data.nasaData.title
-}
+let setPhotoOfTheDay = (data) => {
+  description.textContent = data.nasaData.explanation;
+  photoOfTheDay.src = data.nasaData.hdurl;
+  potdHref.setAttribute("href", data.nasaData.hdurl);
+  title.textContent = data.nasaData.title;
+};
 
-const getPotd = async ()=>{
-    const potdPath = '/potd'
-    const potdData = await fetchDataFromApi(potdPath)
-   setPhotoOfTheDay(potdData)
-}
+const getPotd = async () => {
+  const potdPath = "/potd";
+  const potdData = await fetchDataFromApi(potdPath);
+  setPhotoOfTheDay(potdData);
+};
+/*  */
+
+// click rover button to choose rover, then fetch info then pass parameters and get photos
+const getRoverInfo = async () => {
+  console.log("klik");
+  let roverInfoPath = `mars/info`;
+  let roverInfo = await fetchDataFromApi(roverInfoPath);
+  console.log("rower info", roverInfo);
+};
 
 //get photos from rovers
-const getRoverData = async (rover, sol)=>{
-    let roverPath = `/mars/?sol=${sol}&rover=${rover}`
-     const roverData = await fetchDataFromApi(roverPath)
-     setRoverData(roverData)
- 
-}
+const getRoverData = async (rover, sol) => {
+  let roverPath = `/mars/?sol=${sol}&rover=${rover}`;
+  const roverData = await fetchDataFromApi(roverPath);
+  setRoverData(roverData);
+};
 
+const setRoverData = (data) => {
+  roverGallery.innerHTML = "";
+  data.marsData.photos.forEach((item) => {
+    let imageContainer = document.createElement("a");
+    imageContainer.classList.add("rover-image-container");
+    imageContainer.setAttribute("href", item.img_src);
+    imageContainer.setAttribute("target", "_blank");
+    imageContainer.innerHTML = `<p> id ${item.id}</p>`;
+    imageContainer.innerHTML = `<p> earth date ${item.earth_date}</p> <p> sol ${item.sol}</p>`;
 
- const setRoverData = data =>{
-     roverGallery.innerHTML=''
-     data.marsData.photos.forEach((item)=>{
-        console.log('item',item)
-    
-       let imageContainer = document.createElement('a')
-           imageContainer.classList.add('rover-image-container')
-           imageContainer.setAttribute('href',item.img_src )
-           imageContainer.setAttribute('target', '_blank')
-           imageContainer.innerHTML = `<p> id ${item.id}</p>`;
-           imageContainer.innerHTML = `<p> earth date ${item.earth_date}</p> <p> sol ${item.sol}</p>`;
-      
-       roverGallery.appendChild(imageContainer);
-       let image = document.createElement('IMG')
-         image.classList.add('rover-image')
-         image.src = item.img_src
-         imageContainer.appendChild(image)
-    }) 
-}
-
+    roverGallery.appendChild(imageContainer);
+    let image = document.createElement("IMG");
+    image.classList.add("rover-image");
+    image.src = item.img_src;
+    imageContainer.appendChild(image);
+  });
+};
 
 getPotd();
 
-curiosityBtn.addEventListener('click',()=>{getRoverData('curiosity',sol)})
-opportunityBtn.addEventListener('click',()=>{getRoverData('opportunity',sol)})
-spiritBtn.addEventListener('click',()=>{getRoverData('spirit',sol)})//tu przekazać rover
+curiosityBtn.addEventListener("click", () => {
+  getRoverData("curiosity", 1000);
+});
+opportunityBtn.addEventListener("click", () => {
+  getRoverData("opportunity", 1090);
+});
+spiritBtn.addEventListener("click", () => {
+  getRoverData("spirit", 2000);
+}); //tu przekazać rover
+setSolBtn.addEventListener("click", () => {
+  getRoverInfo();
+});
--- routes/mars.js
+++ routes/mars.js
@@ -3,14 +3,26 @@ const router = express.Router();
 const fetch = require("node-fetch");
 const dotenv = require("dotenv").config();
 
-//manifest https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=
+
 
 const NASA_API_KEY = `${process.env.NASA_API}`;
+
+
 
 const urlBuilder = async (roverName, sol) => {
   let MarsRoversPhotosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&api_key=${NASA_API_KEY}`;
   return MarsRoversPhotosUrl;
 };
+
+
+
+const getMaxSol = async rover =>{
+  //change name
+  //get number of sol availible and chose one
+  const manifestUrl = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}/?api_key=${NASA_API_KEY}`
+  let maxSol = await FetchDataFromApi(manifestUrl)
+  return maxSol.photo_manifest.max_sol
+} 
 
 //fetch data from api, pass the url as an argument
 const FetchDataFromApi = async (url) => {
@@ -19,14 +31,33 @@ const FetchDataFromApi = async (url) => {
   return data;
 };
 
+const fetchMarsData = async (rover, sol) =>{
+  let url = await urlBuilder(rover, sol);
+  return await FetchDataFromApi(url);
+}
+//fetch info
+router.get('/info', async (req,res)=>{
+  try{
+   let roverInfo = await getMaxSol('curiosity')
+
+   res.send({roverInfo: roverInfo})
+  }
+  catch{
+    res.json({ message: err });
+  }
+})
+
+
+
 //fetch data
 router.get("/", async (req, res) => {
   try {
     let rover = req.query.rover;
     let sol = req.query.sol;
-    let url = await urlBuilder(rover, sol);
-    let marsData = await FetchDataFromApi(url);
-    res.send({ marsData: marsData });
+    let maxSol = await getMaxSol(rover)
+    let marsData = await fetchMarsData(rover,sol)
+   
+    res.send({ marsData: marsData});
   } catch {
     res.json({ message: err });
   }
